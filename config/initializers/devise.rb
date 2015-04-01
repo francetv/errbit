b71ff482eead40b171ad52ec5b2acb256450cb7d
Nassim Ait boufrad <nassim.aitboufrad@francetv.fr>
2015-04-01 16:15:45 +0200

    maj ldap

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,4 +265,7 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # LDAP requirements
+  config.ldap_create_user = true
 end
